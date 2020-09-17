f023c8e8dde95b79444eaac51d63b686fa886129
VilhjalmurJE <vilhjalmur8989@gmail.com>
2020-09-17 10:41:42 +0000

    added algorithm

---
+++ TileTraveller.py
@@ -0,0 +1,8 @@
+# Vilhjálmur, Katrín 
+# Leikmaðurinn er staddur í 3x3 grid, byrjar á 1,1 og hann vinnur ef hann fer á 3,1
+# Leikmaðurinn getur valið hvort hann vilji fara suður, vestur, norður og austur
+# Leikmaðurinn byrjar á að geta bara farið norður. Ef leimaðurinn skrifar inn átt sem ekki er hægt að fara í á að koma invalid.
+#
+
+
+
